8539f8326fe90a42ee9ae024a825ce74c2c4ba67
Edson Salvador <esalvador032@gmail.com>
2026-07-10 22:14:20 -0500

    fix: add dictionary and format

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -2,11 +2,9 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ligo_challenge/app/app.dart';
 
 void main() {
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
-    });
+    testWidgets('renders CounterPage', (tester) async {});
   });
 }
